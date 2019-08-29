e8d4aa10a126196020b52d2a678222496d11dc06
Kyle <kirvine87@gmail.com>
2019-08-29 13:19:05 +0100

    adds list talking to app

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template lang="html">
-  <h1>Energy</h1>
+  <div class="">
+    <h1>Energy</h1>
+    <div class="">
+      <energies-list></energies-list>
+    </div>
+  </div>
 </template>
 
 <script>
+import EnergiesList from './components/EnergiesList.vue'
 export default {
   name: 'app',
   data() {
@@ -14,6 +20,9 @@ export default {
     fetch('https://api.carbonintensity.org.uk/generation')
     .then(res => res.json())
     .then(data => this.energies = data)
+  },
+  components: {
+    "energies-list": EnergiesList
   }
 }
 </script>
---
+++ src/components/EnergiesList.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <div class="">
+    <ul>
+      <p>Hi im text</p>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'energies-list'
+}
+</script>
+
+<style lang="css" scoped>
+</style>
