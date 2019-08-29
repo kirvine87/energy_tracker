73c7f3a01b6ffca051669e819050ccfde72bf577
Kyle <kirvine87@gmail.com>
2019-08-29 12:13:37 +0100

    adds fetch data

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="html">
+  <h1>Energy</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      energies: {}
+    }
+  },
+  mounted() {
+    fetch('https://api.carbonintensity.org.uk/generation')
+    .then(res => res.json())
+    .then(data => this.energies = data)
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="css" scoped>
 </style>
---
+++ src/components/ListItem.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <div class="">
+    <ul>
+      <list-component></list-componenet>
+    </ul>
+  </div>
+
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
